9fb8cf997431e9383886dfebea8b2f1dd19566fd
Logathasan Keshani <keshanil.23@uom.lk>
2026-06-16 17:26:09 +0530

    feat: update login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,70 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  return (
-    <div>
-      <h1>Login Page</h1>
+  const navigate = useNavigate();
 
-      <Link to="/register">
-        Go to Register
-      </Link>
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        form
+      );
+
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+
+      alert(res.data.message);
+      navigate("/dashboard");
+    } catch (err) {
+      alert(err.response?.data?.message || "Cannot connect to backend");
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>FindU Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p>
+        Don't have an account? <Link to="/register">Register here</Link>
+      </p>
     </div>
   );
 }
